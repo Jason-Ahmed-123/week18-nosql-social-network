406d00fde7015035ea36f5572c06945f6a4c2c94
Jason Ahmed <jason_ahmed@yahoo.com>
2020-09-17 14:48:54 -0700

    started user-controller file

---
+++ controllers/user-controller.js
@@ -0,0 +1,25 @@
+const { User } = require('../models')
+
+const userController = {
+    //get all users
+    getAllUsers(req,res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+         })
+         .populate ({
+             path: 'friends',
+             select: '-__v'
+         })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        });
+      },
+
+
+
+module.exports = userController
