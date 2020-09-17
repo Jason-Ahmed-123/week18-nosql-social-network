11d3c5065a70fb7b6873881e672aad8d3717c942
Jason Ahmed <jason_ahmed@yahoo.com>
2020-09-17 16:16:44 -0700

    added reactions model file

---
+++ models/reactions.js
@@ -0,0 +1,39 @@
+// dependencies:
+const { Schema, model, Types } = require('mongoose');
+// Moment JS will track time:
+const moment = require('moment');
+
+
+// Reaction (Schema Only!):
+const ReactionSchema = new Schema(
+    {
+      // Use Mongoose's ObjectId data type
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
+
+
+  // Export Reaction model schema
+module.exports = ReactionSchema;
--- models/thought.js
+++ models/thought.js
@@ -5,36 +5,8 @@ const { Schema, model, Types } = require('mongoose');
 // Moment JS will track time:
 const moment = require('moment');
 
+const ReactionSchema = require('./reactions');
 
-// Reaction (Schema Only!):
-const ReactionSchema = new Schema(
-    {
-      // Use Mongoose's ObjectId data type
-      reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId()
-      },
-      reactionBody: {
-        type: String,
-        required: true,
-        maxlength: 280
-      },
-      username: {
-        type: String,
-        required: true
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-      }
-    },
-    {
-      toJSON: {
-        getters: true
-      }
-    }
-  );
 
   
 // Thought Schema:
