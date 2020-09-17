f570eb3476fee8fa31e63f065c2c49a2289281ab
Jason Ahmed <jason_ahmed@yahoo.com>
2020-09-17 13:54:04 -0700

    set up models for user, thought, reaction

---
+++ models/index.js
@@ -0,0 +1,8 @@
+// Requires User path:
+const User = require('./User');
+
+// Requires Thought path:
+const Thought = require('./Thought');
+
+
+module.exports = { User, Thought };
---
+++ models/thought.js
@@ -0,0 +1,78 @@
+// THOUGHT MODEL (Used the "pizza-hunt" module as guide)
+
+// dependencies:
+const { Schema, model, Types } = require('mongoose');
+// Moment JS will track time:
+const moment = require('moment');
+
+
+// Reaction (Schema Only!):
+const ReactionSchema = new Schema(
+    {
+      // Use Mongoose's ObjectId data type
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
+
+  
+// Thought Schema:
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    // The user that created this thought
+    username: {
+        type: Schema.Types.String,
+        required: true,
+        ref: 'User'
+    },
+    // These are like replies. Array of nested documents created with the reactionSchema:
+    reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+          getters: true
+        },
+        id: false
+    }
+);
+
+
+const Thought = model('Thought', ThoughtSchema)
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reaction.length;
+});
+
+module.exports = Thought;
